f21d174fb08c587dcf8e22875a99cac9b3dcf8cc
Usuário <usuario@MacBook-Pro-de-Vicente.local>
2021-03-06 10:30:05 -0300

    alteracoes projeto Udemy rest with spring Boot

---
+++ 02 RestWithSpringBootUdemy Scaffold Calculator/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id,address,first_name,gender,last_name) VALUES 
+(1,'Uberlandia - Minas Gerais - Brasil','Leandro','Male','Costa')
+,(2,'Uberlandia - Minas Gerais - Brasil','Gabriela','Female','Costa')
+,(7,'Uberlandia - Minas Gerais - Brasil','Fernanda','Female','Cardoso da Silva')
+,(8,'Uberlandia - Minas Gerais - Brasil','Pedro','Male','da Silva')
+,(9,'Uberlandia - Minas Gerais - Brasil','Joao','Male','Lima')
+;
